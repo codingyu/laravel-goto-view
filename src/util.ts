624e957444c88ab9ed38aef3a6f4bcaa46c6ec22
cy <everceyu@gmail.com>
2018-01-29 21:22:12 +0800

    🤔

--- src/util.ts
+++ src/util.ts
@@ -5,7 +5,8 @@ import * as fs from "fs";
 
 export function getFilePath(text:string, document:TextDocument) {
     let config = workspace.getConfiguration('laravel_goto_view');
-    let filePath = workspace.getWorkspaceFolder(document.uri).uri.fsPath + config.folder[0].path + "/" + text.replace(/\./g,'/').replace(/\"|\'/g,'') + ".blade.php";
+    let workspaceFolder = workspace.getWorkspaceFolder(document.uri).uri.fsPath;
+    let filePath = workspaceFolder + config.folder[0].path + "/" + text.replace(/\./g,'/').replace(/\"|\'/g,'') + ".blade.php";
     if(fs.existsSync(filePath)){
         return filePath;
     }else{
@@ -15,17 +16,33 @@ export function getFilePath(text:string, document:TextDocument) {
 
 export function getFilePaths(text:string, document:TextDocument): any {
     let config = workspace.getConfiguration('laravel_goto_view');
+    let workspaceFolder = workspace.getWorkspaceFolder(document.uri).uri.fsPath;
+    text = text.replace(/\"|\'/g,'');
     let result = [];
-    for (let item of config.folder) {
-        let showPath = item.path + "/" + text.replace(/\./g,'/').replace(/\"|\'/g,'') + ".blade.php";
-        let filePath = workspace.getWorkspaceFolder(document.uri).uri.fsPath + showPath;
+    if (text.indexOf("::") != -1) {
+        let info = text.split('::');
+        let showPath = config.folder[info[0]] + "/" + info[1].replace(/\./g,'/') + ".blade.php";
+        let filePath = workspaceFolder + showPath;
         if(fs.existsSync(filePath)){
             result.push({
-                "name": item.name,
+                "name": info[0],
                 "showPath": showPath,
                 "fileUri": Uri.file(filePath)
             });
         }
+    } else {
+        for (let item in config.folder) {
+            let showPath = config.folder[item] + "/" + text.replace(/\./g,'/') + ".blade.php";
+            let filePath = workspaceFolder + showPath;
+            if(fs.existsSync(filePath)){
+                result.push({
+                    "name": item,
+                    "showPath": showPath,
+                    "fileUri": Uri.file(filePath)
+                });
+            }
+        }
     }
+    
     return result;
 }
