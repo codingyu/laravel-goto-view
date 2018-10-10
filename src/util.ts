6f2b7cbf99c1e3756bc7a759e5a4de21f41a9840
Yu <everceyu@gmail.com>
2018-10-10 22:05:11 +0800

    😂

--- src/util.ts
+++ src/util.ts
@@ -2,23 +2,25 @@
 
 import { workspace, TextDocument, Uri } from 'vscode';
 import * as fs from "fs";
+import * as path from "path";
 
-export function getFilePath(text:string, document:TextDocument) {
+export function getFilePath(text: string, document: TextDocument) {
     let paths = getFilePaths(text, document);
     return paths.length > 0 ? paths[0] : null;
 }
 
-export function getFilePaths(text:string, document:TextDocument): any {
+export function getFilePaths(text: string, document: TextDocument): any {
+    let paths = scanViewPaths(document);
     let config = workspace.getConfiguration('laravel_goto_view');
     let workspaceFolder = workspace.getWorkspaceFolder(document.uri).uri.fsPath;
-    text = text.replace(/\"|\'/g,'');
+    text = text.replace(/\"|\'/g, '');
     let result = [];
     if (text.indexOf("::") != -1) {
         let info = text.split('::');
         for (let extension in config.extensions) {
-            let showPath = config.folders[info[0]] + "/" + info[1].replace(/\./g,'/') + config.extensions[extension];
+            let showPath = paths[info[0]] + "/" + info[1].replace(/\./g, '/') + config.extensions[extension];
             let filePath = workspaceFolder + showPath;
-            if(fs.existsSync(filePath)){
+            if (fs.existsSync(filePath)) {
                 result.push({
                     "name": info[0],
                     "showPath": showPath,
@@ -27,11 +29,11 @@ export function getFilePaths(text:string, document:TextDocument): any {
             }
         }
     } else {
-        for (let item in config.folders) {
+        for (let item in paths) {
             for (let extension in config.extensions) {
-                let showPath = config.folders[item] + "/" + text.replace(/\./g,'/') + config.extensions[extension];
+                let showPath = paths[item] + "/" + text.replace(/\./g, '/') + config.extensions[extension];
                 let filePath = workspaceFolder + showPath;
-                if(fs.existsSync(filePath)){
+                if (fs.existsSync(filePath)) {
                     result.push({
                         "name": item,
                         "showPath": showPath,
@@ -62,7 +64,17 @@ export function readText(uri: Uri) {
     });
 }
 
-export function scanViewPaths() {
-    let config = workspace.getConfiguration('laravel_goto_view');
-    console.log(config.folders);
+export function scanViewPaths(document: TextDocument) {
+    let configFolders = workspace.getConfiguration('laravel_goto_view.folders');
+    let folders = {};
+    Object.assign(folders, configFolders);
+    let workspaceFolder = workspace.getWorkspaceFolder(document.uri).uri.fsPath;
+    let modulePath = path.join(workspaceFolder, 'Modules');
+    fs.readdirSync(modulePath).forEach(element => {
+        let file = path.join(modulePath, element);
+        if (fs.statSync(file).isDirectory()) {
+            folders[element.toLocaleLowerCase()] = "/Modules/" + element + "/resources/views";
+        }
+    });
+    return folders;
 }
