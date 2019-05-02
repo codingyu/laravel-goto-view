84bcc8202f7bf77823379dfe59e2f5736fb76226
Muah <ctf0@users.noreply.github.com>
2019-05-02 02:59:33 +0200

    Update hoverProvider.ts

--- src/providers/hoverProvider.ts
+++ src/providers/hoverProvider.ts
@@ -5,21 +5,20 @@ import * as util from '../util';
 
 export class HoverProvider implements vscode.HoverProvider {
     provideHover(doc: vscode.TextDocument, pos: vscode.Position, token: vscode.CancellationToken): vscode.ProviderResult<vscode.Hover> {
-        let reg = /(?<=view\(|@include\(|@extends\(|@component\()(['"])[^'"]*\1/;
-        let config = vscode.workspace.getConfiguration('laravel_goto_view');
+        let reg = /(?<=trans\(|__\(|@lang\()(['"])[^'"]*\1/;
         let linkRange = doc.getWordRangeAtPosition(pos, reg);
-        if (linkRange) {
-            let filePaths = util.getFilePaths(doc.getText(linkRange), doc);
-            let workspaceFolder = vscode.workspace.getWorkspaceFolder(doc.uri);
-            if (filePaths.length > 0) {
-                let text: string = "";
-                for (let i in filePaths) {
-                    text += config.folderTip ? `\`${filePaths[i].name}\`` : '';
-                    text += ` [${workspaceFolder.name + filePaths[i].showPath}](${filePaths[i].fileUri})  \r`;
-                }
-                return new vscode.Hover(new vscode.MarkdownString(text));
+
+        if (!linkRange) return
+
+        let filePaths = util.getFilePaths(doc.getText(linkRange), doc);
+        let workspaceFolder = vscode.workspace.getWorkspaceFolder(doc.uri);
+        if (filePaths.length > 0) {
+            let text: string = "";
+            for (let i in filePaths) {
+                text += `\`${filePaths[i].name}\``;
+                text += ` [${workspaceFolder.name + filePaths[i].showPath}](${filePaths[i].fileUri})  \r`;
             }
-        }
-        return;
+            return new vscode.Hover(new vscode.MarkdownString(text));
+        };
     }
 }
