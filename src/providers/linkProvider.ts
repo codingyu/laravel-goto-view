00396902e5ead8840946eb7a1ecd6ef9f84492ad
Muah <ctf0@users.noreply.github.com>
2019-05-02 02:59:28 +0200

    Update linkProvider.ts

--- src/providers/linkProvider.ts
+++ src/providers/linkProvider.ts
@@ -6,27 +6,26 @@ import * as util from '../util';
 export class LinkProvider implements vscode.DocumentLinkProvider {
     public provideDocumentLinks(doc: vscode.TextDocument, token: vscode.CancellationToken): vscode.ProviderResult<vscode.DocumentLink[]> {
         let documentLinks = [];
-        let config = vscode.workspace.getConfiguration('laravel_goto_view');
         let index = 0;
-        let reg = /(?<=view\(|@include\(|@extends\(|@component\()(['"])[^'"]*\1/g;
-        if (config.quickJump) {
-            while (index < doc.lineCount) {
-                let line = doc.lineAt(index);
-                let result = line.text.match(reg);
-                if (result != null) {
-                    for (let item of result) {
-                        let file = util.getFilePath(item, doc);
-                        if (file != null) {
-                            let start = new vscode.Position(line.lineNumber, line.text.indexOf(item));
-                            let end = start.translate(0, item.length);
-                            let documentlink = new vscode.DocumentLink(new vscode.Range(start, end), file.fileUri);
-                            documentLinks.push(documentlink);
-                        };
-                    }
+        let reg = /(?<=trans\(|__\(|@lang\()(['"])[^'"]*\1/g;
+
+        while (index < doc.lineCount) {
+            let line = doc.lineAt(index);
+            let result = line.text.match(reg);
+            if (result != null) {
+                for (let item of result) {
+                    let file = util.getFilePath(item, doc);
+                    if (file != null) {
+                        let start = new vscode.Position(line.lineNumber, line.text.indexOf(item));
+                        let end = start.translate(0, item.length);
+                        let documentlink = new vscode.DocumentLink(new vscode.Range(start, end), file.fileUri);
+                        documentLinks.push(documentlink);
+                    };
                 }
-                index++;
             }
+            index++;
         }
+
         return documentLinks;
     }
 }
